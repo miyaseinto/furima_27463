f2855a0dcb8ab6c50df7f111285ab1db904bac69
miyaseinto <cbj2877@yahoo.co.jp>
2020-07-23 12:00:37 +0900

    userテーブルマイグレーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :family_name, presence: true
+  validates :last_name, presence: true
 end
