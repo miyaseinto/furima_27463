48ee8e126a211496a130b342ce8888ac9995fbef
miyaseinto <cbj2877@yahoo.co.jp>
2020-07-26 15:03:33 +0900

    active_hashの導入及びビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,11 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :detail_category, :detail_status, :delivery_burden, :delivery_area, :delivery_days, :selling_price)
   end
 end
