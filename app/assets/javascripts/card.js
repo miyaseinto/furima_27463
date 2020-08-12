bb22da318b042fc2bbabe4dbba9f0cca10bd754d
miyaseinto <cbj2877@yahoo.co.jp>
2020-08-12 21:20:48 +0900

    調整

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,39 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const pay = () => {
+    Payjp.setPublicKey("pk_test_f085bcee537ecca631c581b6");
+    const form = document.getElementById("charge-form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-const pay = () => {
-  Payjp.setPublicKey("pk_test_f085bcee537ecca631c581b6");
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+      const formResult = document.getElementById("charge-form");
+      const formData = new FormData(formResult);
 
-    const formResult = document.getElementById("charge-form");
-    const formData = new FormData(formResult);
+      const card = {
+        number: formData.get("purchase_addresse[number]"),
+        cvc: formData.get("purchase_addresse[cvc]"),
+        exp_month: formData.get("purchase_addresse[exp_month]"),
+        exp_year: `20${formData.get("purchase_addresse[exp_year]")}`,
+      };
+      Payjp.createToken(card, (status, response) => {
+        if (status === 200) {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
 
-    const card = {
-      number: formData.get("purchase_addresse[number]"),
-      cvc: formData.get("purchase_addresse[cvc]"),
-      exp_month: formData.get("purchase_addresse[exp_month]"),
-      exp_year: `20${formData.get("purchase_addresse[exp_year]")}`,
-    };
-    console.log(card)
-    Payjp.createToken(card, (status, response) => {
-      console.log(response)
-      if (status === 200) {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-
-        document.getElementById("purchase_addresse_number").removeAttribute("name");
-        document.getElementById("purchase_addresse_cvc").removeAttribute("name");
-        document.getElementById("purchase_addresse_exp_month").removeAttribute("name");
-        document.getElementById("purchase_addresse_exp_year").removeAttribute("name");
-        document.getElementById("charge-form").submit();
-        document.getElementById("charge-form").reset();
-      } else {
-      }
+          document.getElementById("purchase_addresse_number").removeAttribute("name");
+          document.getElementById("purchase_addresse_cvc").removeAttribute("name");
+          document.getElementById("purchase_addresse_exp_month").removeAttribute("name");
+          document.getElementById("purchase_addresse_exp_year").removeAttribute("name");
+          document.getElementById("charge-form").submit();
+          document.getElementById("charge-form").reset();
+        } else {
+        }
+      });
     });
-  });
-};
-window.addEventListener("load", pay);
-
+  };
+  window.addEventListener("load", pay);
+});
 
